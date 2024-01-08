df803233a0240cece19c1e56b38ad13019eefd27
alhatpratik <alhatpratik95@gmail.com>
2024-01-09 00:31:24 +0530

    Code of fetching last Central Admitted Record

--- src/main/java/project/EntityDAO/COPDLogic.java
+++ src/main/java/project/EntityDAO/COPDLogic.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import project.Entity.Central_Admit;
 import project.Entity.Patient;
 import project.model.Summary;
 import project.repositories.Central_OPDRepo;
@@ -19,6 +20,9 @@ public class COPDLogic {
 	@Autowired
 	Central_OPDRepo opdi;
 
+	@Autowired
+	CentralAdmitLogic central_admit_logic;
+	
 	public List<Patient> fetchCOPD(LocalDate d)
 	{
 		System.out.println("11");
@@ -39,6 +43,12 @@ public class COPDLogic {
 		Patient pt = opdi.getLastRecord();
 		if(pt !=null)
 		{
+			// here we want last admitted records Yearly no. to show on dashboard count of total admitted patient
+			// So as like another showing parameters we have fetched lastAdmitted record and changed value of last COPD Record's old no value
+			
+			Central_Admit last_Admitted_Record = central_admit_logic.getLastAdmittedAdmitRecord();
+			double last_Admitted_Record_yr_no = last_Admitted_Record.getYearly_no();
+			pt.setOld_no(last_Admitted_Record_yr_no);
 			System.out.println(pt.getName()+"  "+pt.getDate());
 		}
 		
